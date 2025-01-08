501c42a7bc7d966aa16157c373fafe5213e3ba5e
beckyshaz <sharonsakah8@gmail.com>
2025-01-08 08:53:44 +0000

    creating buttons to control the algorithm

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is my piortfolio project based on algorithm visualizer that is musicall
--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 const n=50;
 const array=[];
 
-for(let i=0; i<n; i++){
-    array[i] = Math.random()
-
+function start(){
+    for(let i=0; i<n; i++){
+        array[i] = Math.random()
+    }
+    showBars();
 }
 
-
-for(let i=0; i<array.length; i++){
-    const bars=document.createElement("div");
-    bars.style.height=array[i]*100+"%";
-    bars.classList.add("bars");
-    container.appendChild(bars);
+function bubbleSort(array){
+    do{
+        var swapped=false;
+        for(let i=0; i<array.length - 1; i++){
+            if(array[i] > array[i + 1]){
+                let temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    }while(swapped);
 }
+
+function play(){
+    bubbleSort(array);
+    showBars();
+}
+
+function showBars(){
+    container.innerHTML="";
+    for(let i=0; i<array.length; i++){
+        const bars=document.createElement("div");
+        bars.style.height=array[i]*100+"%";
+        bars.classList.add("bars");
+        container.appendChild(bars);
+    }
+}
+
+start();
